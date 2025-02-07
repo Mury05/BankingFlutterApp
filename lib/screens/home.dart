13c7b4af20140faf73bf2bd9716fd9318abaa2c5
Mury05 <murielleodilone@gmail.com>
2025-02-07 11:18:00 +0100

    Finalisation de la page home, onBoarding terminé ✅

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:banking_flutter_app/widgets/homewidget.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -8,94 +8,37 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final PageController _pageController = PageController();
+
+  final List<Map<String, String>> carouselItems = [
+    {
+      "image": "image1.jpg",
+      "title": "Step into\nthe banking\nfuture now",
+      "subtitle":
+          "We will do everything but count coins.\nFaster, easier, and more convenient."
+    },
+    {
+      "image": "image2.jpg",
+      "title": "Your money,\nyour control",
+      "subtitle":
+          "Manage your finances seamlessly\nwith our cutting-edge solutions."
+    },
+    {
+      "image": "image4.jpg",
+      "title": "Security &\nInnovation",
+      "subtitle":
+          "Banking redefined for the digital age.\nExperience secure and smart banking."
+    }
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          // Fond avec une image
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("/image1.jpg"), // Remplace avec ton image
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
+      body: CarrouselOnBoarding(
+          pageController: _pageController, carouselItems: carouselItems),
 
-          // Contenu
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // Bouton Skip en haut à droite
-                Align(
-                  alignment: Alignment.topRight,
-                  child: TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      "SKIP",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: "",
-                      ),
-                    ),
-                  ),
-                ),
-
-                Spacer(),
-
-                // Texte principal
-                Text(
-                  "Step into\nthe banking\nfuture now",
-                  style:TextStyle(
-                    fontSize: 50,
-                    fontWeight: FontWeight.w300,
-                    color: Colors.white,
-                  ),
-                ),
-
-                SizedBox(height: 20),
-
-                // Sous-texte
-                Text(
-                  "We will do everything but count coins.\nFaster, easier, and more convenient.",
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.white70,
-                  ),
-                ),
-
-                SizedBox(height: 60),
-
-                // Ligne décorative
-                Container(
-                  height: 2,
-                  width: 80,
-                  color: Colors.white,
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          color: Colors.grey[200],
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(18.0),
-          child: TextButton(
-            style: TextButton.styleFrom(
-              foregroundColor: Colors.green[900],
-            ),
-            onPressed: () {},
-            child: Text("OPEN AN ACCOUNT >", style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold),),
-          ),
-        ),
-      ),
+      // Bouton "Open an Account"
+      bottomNavigationBar: HomeBottomNavigation(),
     );
   }
 }
