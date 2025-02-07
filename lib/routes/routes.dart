cfa79e71644d33c18c2744ffefcc7cca6bb81e19
Mury05 <murielleodilone@gmail.com>
2025-02-07 11:30:18 +0100

    Ajout de la route de l'ajout

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,3 +1,4 @@
+import 'package:banking_flutter_app/screens/add.dart';
 import 'package:banking_flutter_app/screens/home.dart';
 import 'package:banking_flutter_app/screens/transfer.dart';
 import 'package:flutter/material.dart';
@@ -5,7 +6,8 @@ import 'package:flutter/material.dart';
 class Routes {
   final Map<String, WidgetBuilder> _routes = {
     '/': (context) => const MyHomePage(),
-    '/transfer': (context) => const MyTransferPage()
+    '/transfer': (context) => const MyTransferPage(),
+    '/add': (context) => const MyAddTransactionPage(),
   };
 
   Map<String, WidgetBuilder> getRoutes() => _routes;
