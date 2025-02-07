75755c5831e360c490ed4f5e0c7d84945d79e54b
Mury05 <murielleodilone@gmail.com>
2025-02-07 11:17:11 +0100

    Ajout de la route transfer,de certaines dépendances et de quelques images

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,9 +1,11 @@
 import 'package:banking_flutter_app/screens/home.dart';
+import 'package:banking_flutter_app/screens/transfer.dart';
 import 'package:flutter/material.dart';
 
 class Routes {
   final Map<String, WidgetBuilder> _routes = {
     '/': (context) => const MyHomePage(),
+    '/transfer': (context) => const MyTransferPage()
   };
 
   Map<String, WidgetBuilder> getRoutes() => _routes;
